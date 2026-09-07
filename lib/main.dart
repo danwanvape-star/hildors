5a95452fef8ea46a642295e2aaba7a79b55f738b
HILDORS Team <danwanvape@gmail.com>
2026-09-07 16:37:44 +0800

    fix: add software video decoding fallback

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:video_player_media_kit/video_player_media_kit.dart';
 
 import 'src/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  VideoPlayerMediaKit.ensureInitialized(android: true, iOS: true);
   runApp(const CockpitApp());
 }
